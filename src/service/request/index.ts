acd53e9d560d9c5c88cb16cf02249ab38220164c
youthOfMe <2040690180@qq.com>
2024-03-03 22:54:21 +0800

    feat: 封装网络请求request

--- src/service/request/index.ts
+++ src/service/request/index.ts
@@ -1,12 +1,13 @@
 import axios from 'axios'
 import type { AxiosInstance, AxiosRequestConfig } from 'axios'
+import type { HYRequestConfig } from './type'
 
 // 拦截器: 可以进行蒙版loading/配置token/修改配置
 class HYRequest {
     instance: AxiosInstance
     
     // request实例 => axios实例
-    constructor(config: AxiosRequestConfig) {
+    constructor(config: HYRequestConfig) {
         this.instance = axios.create(config)
         
         // 每个instance实例都添加拦截器
@@ -26,5 +27,23 @@ class HYRequest {
         })
     }
 
+    // 封装网络请求的方法
+    request<T = any>(config: HYRequestConfig<T>) {
+        // 单次请求的成功拦截成功处理 定制化处理
+        if (config.inteceptors?.requestSuccessFn) {
+            config = config.inteceptors.requestSuccessFn(config)
+        }
+
+        return new Promise<T>((resolve, reject) => {
+            this.instance.request<any, T>(config).then(res => {
+                if (config.inteceptors?.responseSuccessFn) {
+                    res = config.inteceptors.responseSuccessFn(res)
+                }
+                resolve(res)
+            }).catch(err => {
+                reject(err)
+            })
+        })
+    }
 
 }
